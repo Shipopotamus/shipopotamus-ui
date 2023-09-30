ba35f00b719ab6cf109e32be458a438a4070fb33
Marcin Słowiak <m.slowiak@smartrecruiters.com>
2023-09-30 21:20:35 +0200

    Add welcome page with redirects to login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
@@ -9,10 +7,4 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'shipopotamus-ui';
-
-  constructor(private router: Router) { }
-
-  navigateToLogin() {
-    this.router.navigate(['/login']);
-  }
 }
