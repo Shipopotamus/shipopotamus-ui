c082f8104cf1d13647def415c7e51fb6dff8579d
Marcin Słowiak <m.slowiak@smartrecruiters.com>
2023-10-01 21:50:41 +0200

    Implement sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'sign-up', component: SignUpComponent },
 ];
 
 @NgModule({
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient) {}
+
+  onSubmit() {
+    const userData = {
+      email: this.email,
+      password: this.password
+    };
+
+    this.http.post('http://localhost:8080/sign-up', userData).subscribe(
+      (response) => {
+        console.log('Sign in successful', response);
+      },
+      (error) => {
+        console.error('Sign in failed', error);
+      }
+    );
+  }
 }
