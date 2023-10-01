bd7fe887d5f62e7d8d1bc7018076f130d8c7650a
Marcin Słowiak <m.slowiak@smartrecruiters.com>
2023-10-01 21:54:56 +0200

    Move to welcome after log-in

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class SignInComponent {
   email: string = '';
   password: string = '';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   onSubmit() {
     const userData = {
@@ -21,6 +22,7 @@ export class SignInComponent {
     this.http.post('http://localhost:8080/sign-in', userData).subscribe(
       (response) => {
         console.log('Sign in successful', response);
+        this.router.navigate(['/welcome']);
       },
       (error) => {
         console.error('Sign in failed', error);
